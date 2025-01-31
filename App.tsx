02e6fa0cdf7693b7cffe3d56f48167f1c63087e9
Harshitaa Kashyap <suzimayisbae@gmail.com>
2025-02-01 01:08:43 +0530

    feat: basic map integration

--- App.tsx
+++ App.tsx
@@ -7,7 +7,25 @@ import RideDetailsSelector from "./components/RideDetailsSelector";
 import MainNavBar from "./components/MainNavBar";
 import HomeScreen from "./screens/HomeScreen";
 import {
-    useFonts,
+  useFonts,
+  NunitoSans_200ExtraLight,
+  NunitoSans_200ExtraLight_Italic,
+  NunitoSans_300Light,
+  NunitoSans_300Light_Italic,
+  NunitoSans_400Regular,
+  NunitoSans_400Regular_Italic,
+  NunitoSans_600SemiBold,
+  NunitoSans_600SemiBold_Italic,
+  NunitoSans_700Bold,
+  NunitoSans_700Bold_Italic,
+  NunitoSans_800ExtraBold,
+  NunitoSans_800ExtraBold_Italic,
+  NunitoSans_900Black,
+  NunitoSans_900Black_Italic,
+} from "@expo-google-fonts/nunito-sans";
+
+export default () => {
+  let [fontsLoaded] = useFonts({
     NunitoSans_200ExtraLight,
     NunitoSans_200ExtraLight_Italic,
     NunitoSans_300Light,
@@ -22,32 +40,14 @@ import {
     NunitoSans_800ExtraBold_Italic,
     NunitoSans_900Black,
     NunitoSans_900Black_Italic,
-} from "@expo-google-fonts/nunito-sans";
+  });
 
-export default () => {
-    let [fontsLoaded] = useFonts({
-        NunitoSans_200ExtraLight,
-        NunitoSans_200ExtraLight_Italic,
-        NunitoSans_300Light,
-        NunitoSans_300Light_Italic,
-        NunitoSans_400Regular,
-        NunitoSans_400Regular_Italic,
-        NunitoSans_600SemiBold,
-        NunitoSans_600SemiBold_Italic,
-        NunitoSans_700Bold,
-        NunitoSans_700Bold_Italic,
-        NunitoSans_800ExtraBold,
-        NunitoSans_800ExtraBold_Italic,
-        NunitoSans_900Black,
-        NunitoSans_900Black_Italic,
-    });
-
-    if (!fontsLoaded) {
-        return <Text>Font loading error</Text>;
-    } else {
-        return (
-            <View style={styles.container}>
-                {/* <PreviousTripsCompressed trip={rideData[0]} />
+  if (!fontsLoaded) {
+    return <Text>Font loading error</Text>;
+  } else {
+    return (
+      <View style={styles.container}>
+        {/* <PreviousTripsCompressed trip={rideData[0]} />
                 <MainNavBar
                     variant={0}
                     bottomNavItems={bottomNavItems}
@@ -65,17 +65,17 @@ export default () => {
                     iconPath={require("./assets/smiling-emoji.png")}
                     onPress={() => console.log("Details pressed")}
                 /> */}
-                <HomeScreen />
-            </View>
-        );
-    }
+        <HomeScreen />
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
