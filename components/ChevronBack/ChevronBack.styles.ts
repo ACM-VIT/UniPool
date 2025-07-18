801afa2b0bfcb3a2466e103414a2697445810788
theg <52027622+theg1239@users.noreply.github.com>
2025-07-18 18:01:10 +0530

    feat: added ChevronBack component

---
+++ components/ChevronBack/ChevronBack.styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
+
+export default styles;
