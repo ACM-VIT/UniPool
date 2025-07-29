2aff56d1bad9ee83f62dc12bb91f90def29d8c48
theg <52027622+theg1239@users.noreply.github.com>
2025-07-29 13:50:16 +0530

    feat: add labels for navbar

--- data/BottomNavigationItems.js
+++ data/BottomNavigationItems.js
@@ -3,21 +3,25 @@ const bottomNavItems = [
         iconPath: require("../assets/home-icon.png"),
         route: "home",
         isActive: true,
+        label: "Home",
     },
     {
         iconPath: require("../assets/suitcase-icon.png"),
         route: "trips",
         isActive: false,
+        label: "Trips",
     },
     {
         iconPath: require("../assets/message-icon.png"),
         route: "chat",
         isActive: false,
+        label: "Chat",
     },
     {
         iconPath: require("../assets/user-icon.png"),
         route: "profile",
         isActive: false,
+        label: "Profile",   
     },
 ];
 
