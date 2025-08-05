b47b3548d1740de27046cd6738379fdeb2e499e9
theg <52027622+theg1239@users.noreply.github.com>
2025-08-05 15:50:05 +0530

    fix: remove horizontal padding from brandInfoHeaderRow in ProfileScreen styles

--- screens/ProfileScreen/ProfileScreen.styles.ts
+++ screens/ProfileScreen/ProfileScreen.styles.ts
@@ -10,7 +10,6 @@ const styles = StyleSheet.create({
   },
   brandInfoHeaderRow: {
     width: '100%',
-    paddingHorizontal: width * 0.051,
     paddingTop: height * 0.01,
     marginBottom: height * 0.01,
   },
